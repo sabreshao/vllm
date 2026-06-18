204519981fc504aa800fa2d8c4795f91a9a5c94a
sabreshao <sabre.shao@amd.com>
2026-06-18 07:41:26 +0000

    Use Fangzhou branch.

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,7 @@ DOCKER_BUILDKIT=1 docker build . \
   --build-arg PYTORCH_ROCM_ARCH=gfx950 \
   --build-arg AITER_ROCM_ARCH=gfx950 \
   --build-arg REMOTE_VLLM=1 \
-  --build-arg VLLM_REPO=https://github.com/vllm-project/vllm.git \
-  --build-arg VLLM_BRANCH=b4092176b9bc76839f76b0e91972a52e8b14ea2b \
+  --build-arg VLLM_REPO=https://github.com/Fangzhou-Ai/vllm.git \
+  --build-arg VLLM_BRANCH=dsv4-rocm-atom-full-attention \
   -f docker/dockerfile.rocm_new \
-  -t sabreshao/vllm:aiter_0618
+  -t sabreshao/vllm:aiter_0618_a
